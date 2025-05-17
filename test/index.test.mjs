82f975e736cb20e25ad7ceb9f3d5597ca1c3493b
Lucas <berg@student.ubc.ca>
2025-05-16 20:13:12 -0700

    Removed an unneeded test file

--- test/index.test.mjs
+++
@@ -1,6 +0,0 @@
-import { translate } from "../index.mjs";
-import { saveTranslation } from "../index.mjs";
-
-console.log(await translate("hello", "Spanish"));
-
-saveTranslation("hello", "English", "hola", "Spanish")
